79bfe970ac99a078ad1fa8f03eaa067ba4c6b779
Dylan Ambrose <djambro44@gmail.com>
2020-03-01 17:22:13 -0700

    updating js. can't get next button to work properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-window.onload = function () {
-    $("#timerButton").on("click", start);
-};
+
+$("#timerButton").on("click", start);
+
 
 var intervalId;
 var clockRunning = false;
@@ -62,25 +62,72 @@ function timeConverter(t) {
     return minutes + ":" + seconds;
 }
 
+var correct = 0;
+var currentNumber = 0;
 var allQuestions = [
-    {question: "Q1: What does NFL stand for?", 
-    choices: ["Nation Football Legacy", "New Football League", "National Football Leauge"], 
-    correctAnswer: 2},
-    {question: "Q2: How many point is a touchdown worth?",
-    choices: ["7", "6", "3"],
-    correctAnswer: 1},
-    {question: "Q3: How many teams play in the NFL?",
-    choices: ["12", "18", "22"],
-    correctAnswer: 0},
-    {question: "Q4: What is the only team in NFL histroy to go undefeated and win a superbowl?",
-    choices: ["New England Patriots", "Chicago Bears", "Miami Dolphins"],
-    correctAnswer: 2},
-    {question: "Q5: Who is the only player in NFL history to play on three consecutive Super Bowl championship teams?",
-    choices: ["Tom Brady", "Joe Monntana", "Deion Sanders"],
-    correctAnswer: 2}];
+    {
+        question: "Q1: What does NFL stand for?",
+        choices: ["Nation Football Legacy", "New Football League", "National Football Leauge"],
+        correctAnswer: 2
+    },
+    {
+        question: "Q2: How many point is a touchdown worth?",
+        choices: ["7", "6", "3"],
+        correctAnswer: 1
+    },
+    {
+        question: "Q3: How many teams play in the NFL?",
+        choices: ["12", "18", "22"],
+        correctAnswer: 0
+    },
+    {
+        question: "Q4: What is the only team in NFL histroy to go undefeated and win a superbowl?",
+        choices: ["New England Patriots", "Chicago Bears", "Miami Dolphins"],
+        correctAnswer: 2
+    },
+    {
+        question: "Q5: Who is the only player in NFL history to play on three consecutive Super Bowl championship teams?",
+        choices: ["Tom Brady", "Joe Monntana", "Deion Sanders"],
+        correctAnswer: 2
+    }];
 
-var correct=0;
-var correctNumber = 0;
+
+
+function createQuestion() {
+    for (var i = 0; i < allQuestions[this.currentNumber].choices.length; i++) {
+        document.forms.radioAnswers.elements.choice[i].checked = false;
+    }
+    var question = document.getElementById("question");
+    question.innerHTML = allQuestions[this.currentNumber].question;
+    var point = document.getElementById("point");
+    point.innerHTML = "<span>score: " + correct + "</span>";
+    createChoices();
+}
+function createChoices() {
+    for (var i = 0; i < allQuestions[this.currentNumber].choices.length; i++) {
+        var option = document.getElementById("label" + i);
+        option.innerHTML = allQuestions[this.currentNumber].choices[i];
+    }
+}
+
+function nextQuestion (){
+    $("next").click(function () {
+        alert("it worked");
+    });
+}
+
+function showScore() {
+    document.forms.radioAnswers.style.display = "none";
+    var question = document.getElementById("question");
+    question.style.display = "none";
+    var elt = document.getElementById("point");
+    elt.style.display = "block";
+    var button = document.getElementById("next");
+    button.style.display = "none";
+
+}
+
+window.onload = createQuestion();
 
 
 
