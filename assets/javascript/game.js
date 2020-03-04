816a0bd9867016673bbff0d8dbb4a3ce6e63da3d
Dylan Ambrose <djambro44@gmail.com>
2020-03-03 20:01:19 -0700

    added styling and finished next button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,24 @@
+$(document).ready(function() {
 
-$("#timerButton").on("click", start);
-
-
-var intervalId;
-var clockRunning = false;
-var time = 0;
-
-function reset() {
-
+    $("#timerButton").on("click", start);
+    $("#timerButton").on("click", createQuestion);
+    
+    
+    var intervalId;
+    var clockRunning = false;
+    var time = 0;
+    
+    function reset() {
+        
     time = 100;
 
-
+    
     // DONE: Change the "display" div to "00:00."
     $("#timer").text("00:00");
 
 }
 function start() {
-
+    
     // DONE: Use setInterval to start the count here and set the clock to running.
     if (!clockRunning) {
         intervalId = setInterval(count, 1000);
@@ -24,46 +26,48 @@ function start() {
     }
 }
 function stop() {
-
+    
     // DONE: Use clearInterval to stop the count here and set the clock to not be running.
     clearInterval(intervalId);
     clockRunning = false;
 }
 
 function count() {
-
+    
     // DONE: increment time by 1, remember we cant use "this" here.
     time--;
-
+    
     // DONE: Get the current time, pass that into the timeConverter function,
     //       and save the result in a variable.
     var converted = timeConverter(time);
     console.log(converted);
-
+    
     // DONE: Use the variable we just created to show the converted time in the "display" div.
     $("#timer").text(converted);
 }
 function timeConverter(t) {
-
+    
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-
+    
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
-
+    
     if (minutes === 0) {
         minutes = "00";
     }
     else if (minutes < 10) {
         minutes = "0" + minutes;
     }
-
+    
     return minutes + ":" + seconds;
 }
 
 var correct = 0;
+var incorrect = 0;
 var currentNumber = 0;
+var userGuess;
 var allQuestions = [
     {
         question: "Q1: What does NFL stand for?",
@@ -90,61 +94,88 @@ var allQuestions = [
         choices: ["Tom Brady", "Joe Monntana", "Deion Sanders"],
         correctAnswer: 2
     }];
-
-
-
-function createQuestion() {
-    for (var i = 0; i < allQuestions[this.currentNumber].choices.length; i++) {
-        document.forms.radioAnswers.elements.choice[i].checked = false;
+    
+    
+    
+    function createQuestion() {
+        for (var i = 0; i < allQuestions[currentNumber].choices.length; i++) {
+            document.forms.radioAnswers.elements.choice[i].checked = false;
+        }
+        var question = document.getElementById("question");
+        question.innerHTML = allQuestions[currentNumber].question;
+        var point = document.getElementById("point");
+        point.innerHTML = "<span>score: " + correct + "</span>";
+        createChoices();
     }
-    var question = document.getElementById("question");
-    question.innerHTML = allQuestions[this.currentNumber].question;
-    var point = document.getElementById("point");
-    point.innerHTML = "<span>score: " + correct + "</span>";
-    createChoices();
-}
-function createChoices() {
-    for (var i = 0; i < allQuestions[this.currentNumber].choices.length; i++) {
-        var option = document.getElementById("label" + i);
-        option.innerHTML = allQuestions[this.currentNumber].choices[i];
+    function createChoices() {
+        for (var i = 0; i < allQuestions[currentNumber].choices.length; i++) {
+            var option = document.getElementById("label" + i);
+            option.innerHTML = allQuestions[currentNumber].choices[i];
+        }
     }
-}
+    
+    function nextQuestion (){
+        getUserGuess();
+        checkGuess();
+        currentNumber++;
+        if (currentNumber === allQuestions.length) {
+            alert("Game OVer");
+            console.log(currentNumber);
+        } else {
+            createQuestion();
+        }
+        console.log("user guess: " + userGuess);
+        console.log("correct answer: " + allQuestions[currentNumber].correctAnswer)
+        console.log("Correct guesses: " + correct);
+        console.log("Incorrect guesses: " + incorrect);
+    }
 
-function nextQuestion (){
-    $("next").click(function () {
-        alert("it worked");
-    });
-}
+    $("#next").on("click", nextQuestion);
+    $("radio").on("click", checkGuess());
 
-function showScore() {
-    document.forms.radioAnswers.style.display = "none";
-    var question = document.getElementById("question");
-    question.style.display = "none";
-    var elt = document.getElementById("point");
-    elt.style.display = "block";
-    var button = document.getElementById("next");
-    button.style.display = "none";
+   
+    function getUserGuess() {
+        userGuess = $("input[name='choice']:checked").val();
+    }
 
-}
+    function checkGuess () {
+        if (userGuess === allQuestions[currentNumber].correctAnswer) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+    }
 
-window.onload = createQuestion();
+    function showScore() {
+        document.forms.radioAnswers.style.display = "none";
+        var question = document.getElementById("question");
+        question.style.display = "none";
+        var elt = document.getElementById("point");
+        elt.style.display = "block";
+        var button = document.getElementById("next");
+        button.style.display = "none";
+        
+    }
+    
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+})
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
